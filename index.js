49fe5336c5ef5e2be0d34e4263f63dd4278f7e23
Jim Bulkowski <jim.b@paperelectron.com>
2016-07-29 03:33:33 -0400

    Initial code commit

---
+++ index.js
@@ -0,0 +1,43 @@
+/**
+ * @file index
+ * @author Jim Bulkowski <jim.b@paperelectron.com>
+ * @project Pomegranate-request
+ * @license MIT {@link http://opensource.org/licenses/MIT}
+ */
+
+'use strict';
+var Promise = require('bluebird')
+var request = require('request')
+/**
+ *
+ * @module index
+ */
+
+exports.metadata = {
+  name: 'Request',
+  type: 'factory',
+  param: 'Request'
+}
+
+exports.plugin = {
+  load: function(inject, loaded) {
+    loaded(null, builder)
+  },
+  start: function(done) {
+    done()
+  },
+  stop: function(done) {
+    done()
+  }
+}
+
+function builder(){
+  return function buildRequest(optionsArg){
+    if(optionsArg){
+      return Promise.promisifyAll(
+        request.defaults(optionsArg)
+      , { multiArgs: true })
+    }
+    return Promise.promisifyAll(request)
+  }
+}
